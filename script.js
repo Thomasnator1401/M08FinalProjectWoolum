e2398ed595ab33c10e559ea2c624f55a9761742a
Thomasnator1401 <145989955+Thomasnator1401@users.noreply.github.com>
2023-12-14 14:39:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-$(document).ready(function() {
-    $('#contactForm').submit(function(e) {
-        e.preventDefault();
-        alert('Thank you for reaching out!');
-    });
-});
+// This script adds some interactivity to the page
+
+// Select the profile div element
+var profile = document.getElementById("profile");
+
+// Create a new image element
+var image = document.createElement("img");
+
+// Set the image source to a picture of Thomas Woolum
+image.src = "thomas.jpg";
+
+// Set the image alt attribute to "Thomas Woolum"
+image.alt = "Thomas Woolum";
+
+// Append the image to the profile div
+profile.appendChild(image);
+
+// Select the resort span elements
+var resorts = document.getElementsByClassName("resort");
+
+// Loop through the resort elements
+for (var i = 0; i < resorts.length; i++) {
+  // Add a click event listener to each resort element
+  resorts[i].addEventListener("click", function() {
+    // Toggle the class name of the clicked element between "resort" and "highlight"
+    this.className = (this.className == "resort") ? "highlight" : "resort";
+  });
+}
