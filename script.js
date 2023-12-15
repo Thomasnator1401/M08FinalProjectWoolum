c91e77cc1509a8d7a143cffc54112c3aff6f3519
Thomasnator1401 <145989955+Thomasnator1401@users.noreply.github.com>
2023-12-15 18:59:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,46 @@
-// This script adds some interactivity to the page
-
-// Select the profile div element
-var profile = document.getElementById("profile");
-
-// Create a new image element
-var image = document.createElement("img");
-
-// Set the image source to a picture of Thomas Woolum
-image.src = "thomas.jpg";
-
-// Set the image alt attribute to "Thomas Woolum"
-image.alt = "Thomas Woolum";
-
-// Append the image to the profile div
-profile.appendChild(image);
-
-// Select the resort span elements
-var resorts = document.getElementsByClassName("resort");
-
-// Loop through the resort elements
-for (var i = 0; i < resorts.length; i++) {
-  // Add a click event listener to each resort element
-  resorts[i].addEventListener("click", function() {
-    // Toggle the class name of the clicked element between "resort" and "highlight"
-    this.className = (this.className == "resort") ? "highlight" : "resort";
-  });
-}
+// This function validates the input of the form and sends a POST request to the server
+function addCourse() {
+    // Get the form element by its id
+    var form = document.getElementById("form");
+    // Get the input values from the form
+    var name = form.elements["name"].value;
+    var description = form.elements["description"].value;
+    var subject = form.elements["subject"].value;
+    var credits = form.elements["credits"].value;
+    // Check if the input values are valid
+    if (name && description && subject && credits) {
+      // Create a new XMLHttpRequest object
+      var xhr = new XMLHttpRequest();
+      // Set the request method and the URL
+      xhr.open("POST", "/add_course");
+      // Set the request header to indicate the content type
+      xhr.setRequestHeader("Content-Type", "application/json");
+      // Create a JSON object with the input values
+      var data = JSON.stringify({
+        name: name,
+        description: description,
+        subject: subject,
+        credits: credits,
+      });
+      // Send the request with the data
+      xhr.send(data);
+      // Handle the response
+      xhr.onload = function () {
+        // Check if the status code is 200 (OK)
+        if (xhr.status == 200) {
+          // Parse the response as JSON
+          var response = JSON.parse(xhr.responseText);
+          // Display a success message
+          alert(response.message);
+        } else {
+          // Display an error message
+          alert("Something went wrong!");
+          
+        }
+      };
+    } else {
+      // Display a warning message
+      alert("Please fill in all the fields!");
+    }
+  }
+  
